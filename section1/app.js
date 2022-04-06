66a54f04775c04610aedf9d116887d41b9b92c38
zarry <zarrydie03@gmail.com>
2022-04-06 16:28:22 -0400

    Week three updated

--- section1/app.js
+++ section1/app.js
@@ -1,7 +1,26 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
-console.log(routes.someText);
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
